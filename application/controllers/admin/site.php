dc4f853e5efa63e0ce3566ed55c3285edb6828bc
grupo2d <davsonsantos@gmail.com>
2015-02-19 17:34:38 -0400

    cadatro de portifolio

--- application/controllers/admin/site.php
+++ application/controllers/admin/site.php
@@ -42,6 +42,83 @@ class Site extends CI_Controller {
         }
 		redirect(GD_RAIZ.'site/configuracao');
 	}
+	
+	
+	public function portifolio(){
+        $data = array(
+            'titulo'=>'Lista de Projetos Realizados',
+            'lista'=>$this->site->get_projetos()->result()
+        );
+        $this->load->view('admin/site/lista_portifolio',$data);
+    }
+	
+	function cadastro_projeto(){
+        $acao = base64_decode($this->input->get('acao'));
+        switch ($acao) {
+            case 'novo':
+                $data = array('titulo'=>'Novo Projeto',
+                    'acao'=>$acao,
+                    'botao'=>'Cadastrar',
+                    'cor'=>'success',
+                    'dados'=>'',
+                    'categoria'=>$this->site->get_categoria()->result()
+					);
+                
+                break;
+            case 'editar':
+                $data = array('titulo'=>'Editar Sistema',
+                            'acao'=>$acao,
+                            'botao'=>'Editar',
+                            'cor'=>'warning',
+                            'id'=>$this->input->get('id'),
+                            'dados'=>$this->permissao->get_byIdsistema($this->input->get('id'))->row(),
+							'icon'=>$this->permissao->get_byIcon()->result());
+                break;
+            default:
+                
+                break;       
+        }
+        $this->load->view('admin/site/cad_projeto',$data);
+    }
+
+	function confirmar_projeto(){
+        $acao = $this->input->get_post('acao');
+        switch ($acao) {
+            case 'novo':
+				$config_img = array('diretorio'=>'portifolio','w'=>370,'h'=>240,'ratio'=>FALSE);
+		        $anexo = upload_imagem('logo',$config_img);
+                $dados = array('por_nome'=>  $this->input->post('por_nome'),
+                               'por_descricao'=>$this->input->post('por_descricao'),
+                               'por_img'=>$anexo,
+							   'por_link'=>$this->input->post('por_link'),
+							   'cat_projeto_id'=>$this->input->post('cat_projeto_id'));
+							   
+                $sucesso = $this->site->inserir_projeto($dados);
+				if ($sucesso){
+		            set_msg('msg', 'Dados Inseridos com sucesso', 'sucesso');
+		        }else{
+		            set_msg('msg', 'Erro ao Cadastrar os dados', 'erro');
+		        }
+				redirect(GD_RAIZ.'site/portifolio');
+                break;
+            case 'editar':
+                $dados = array('sis_nome'=>  $this->input->post('sis_nome'),
+                               'sis_descricao'=>$this->input->post('sis_descricao'),
+                               'sis_ativo'=>$this->input->post('sis_ativo'),
+                               'sis_icon'=>$this->input->post('sis_icon'),
+                               'sis_id'=>$this->input->post('id'));
+                $sucesso = $this->permissao->editar_sistema($dados);
+                break;
+            default:
+                
+                break;       
+        }
+        if($sucesso){
+            echo 1;
+        }else{
+            echo 0;
+        }
+    }
     
     
    
--- application/models/admin/site/site_model.php
+++ application/models/admin/site/site_model.php
@@ -17,5 +17,27 @@ class Site_model extends CI_Model {
         $sql = "select * from tb_configuracao";
         return $this->db->query($sql);
     }
+	
+	function get_projetos(){
+		$sql = "SELECT * FROM tb_portifolio p
+				INNER JOIN tb_categoria_projeto c ON p.cat_projeto_id = c.cat_projeto_id";
+		return $this->db->query($sql);
+	}
+	
+	function get_categoria(){
+		$sql = "select * from tb_categoria_projeto";
+		return $this->db->query($sql);
+	}
+	
+	public function inserir_projeto($dados=NULL){
+        if ($dados != NULL):
+            $this->db->insert('tb_portifolio', $dados);
+            if ($this->db->affected_rows()>0):
+               return true;
+            else:
+                return false;
+            endif;
+        endif;
+    }
     
 }
