350a052a65f6723fabfe5867d20c2fc061610691
grupo2d <davsonsantos@gmail.com>
2015-05-19 14:29:50 -0400

    resolvido no problema no cadastro de sistema

--- application/views/admin/permissao/cad_sistema.php
+++ application/views/admin/permissao/cad_sistema.php
@@ -1,4 +1,6 @@
-<?php $this->load->view('admin/layout/header'); ?>
+<?php $this->load->view('admin/layout/header'); 
+error_reporting(0);
+?>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"> </script>
 
 
