8cb1602e9b4bfc91f0b791bff6d2dbda47c58036
grupo2d <davsonsantos@gmail.com>
2015-06-08 13:56:10 -0400

    timezone

--- application/controllers/site/inicio.php
+++ application/controllers/site/inicio.php
@@ -9,8 +9,7 @@ class Inicio extends CI_Controller {
     }
     
     public function index(){
-    //	print_r($_SERVER); 
-	//	echo ip2long($_SERVER['REMOTE_ADDR']);
+    
 		$logo = logo_site();
     	$data = array('titulo'=>$logo['titulo'],'logo'=>$logo['logo'],
 					  'projetos' => $this->site->get_projetos(TRUE)->result(),
